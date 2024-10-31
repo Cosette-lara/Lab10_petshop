da80a442e681e8b4150ef55d02f5e69ae910d5b8
JosueAmbrosio <josue.ambrosio@tecsup.edu.pe>
2024-10-31 14:37:37 -0500

    cuarto

--- src/main/java/com/tecsup/petclinic/repositories/VisitRepository.java
+++ src/main/java/com/tecsup/petclinic/repositories/VisitRepository.java
@@ -1,7 +1,19 @@
 package com.tecsup.petclinic.repositories;
 
+import com.tecsup.petclinic.entities.Pet;
 import com.tecsup.petclinic.entities.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
+import java.util.List;
+
 public interface VisitRepository extends JpaRepository<Visit, Integer> {
+    // Buscar visitas por fecha
+    List<Visit> findByVisitDate(Date visitDate);
+
+    // Buscar visitas por pet (mascota)
+    List<Visit> findByPet(Pet pet);
+
+    // Buscar visitas por pet ID
+    List<Visit> findByPetId(Integer petId);
 }
